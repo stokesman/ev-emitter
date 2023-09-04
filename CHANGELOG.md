a08927ddb1c71eac6c5e7ed1db35bb985f8402f2
Mitchell Austin <mr.fye@oneandthesame.net>
2023-09-04 13:40:03 -0700

    Update types and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,9 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Removed
+
+  - Remove chainable return from methods.
+
 ### Changed
 
   - Rename `emitEvent` to `emit`
   - Convert `emit` second parameter to rest parameter
   - Rename `allOff` to `reset`
   - Support for mix-ins to extend a prototype is through the static `mixin` instead of `prototype`
+  - Invoke callbacks without binding `this`
--- ev-emitter.d.ts
+++ ev-emitter.d.ts
@@ -7,49 +7,55 @@
  */
 export default class EvEmitter {
     /**
-     * Provides public methods.
-     * @returns Public methods of a new instance.
+     * @template {(...args:any[]) => any} T
+     * @typedef {Parameters<T> extends never[] ? never[] : Parameters<T>} OptionalRest
+     */
+    /**
+     * @template {keyof EvEmitter} K
+     * @typedef {(this: EvEmitter, ...args: OptionalRest<EvEmitter[K]> ) => ReturnType<EvEmitter[K]>} BoundMethod
+     */
+    /** @typedef {{[Key in keyof EvEmitter]: BoundMethod<Key>}} EvEmitterMixin */
+    /**
+     * Provides methods that can be assigned to an object to replicate the public interface
+     * of EvEmitter. These are expected to be assigned to individual instances instead of
+     * to a `prototype` as that will produce instances that share state.
+     * @returns Methods bound to a new instance.
      */
     static get mixin(): {
-        on: (type: string, receiver: Receiver<any[]>) => EvEmitter;
-        once: (type: string, receiver: Receiver<any[]>) => EvEmitter;
-        off: (type: string, receiver: Receiver<any[]>) => EvEmitter;
-        emit: (type: string, ...args: any[]) => EvEmitter;
-        reset: () => EvEmitter;
+        on: (this: EvEmitter, type: string, receiver: Receiver<any[]>) => void;
+        once: (this: EvEmitter, type: string, receiver: Receiver<any[]>) => void;
+        off: (this: EvEmitter, type: string, receiver: Receiver<any[]>) => void;
+        emit: (this: EvEmitter, type: string, ...args: any[]) => void;
+        reset: (this: EvEmitter, ...args: never[]) => void;
     };
     /**
      * Adds a callback on a signal.
      * @param {string} type Name of signal.
      * @param {Receiver} receiver Function called when signal dispatches.
-     * @returns The instance.
      */
-    on(type: string, receiver: Receiver): this;
+    on(type: string, receiver: Receiver): void;
     /**
      * Adds a callback on a signal for a single time.
      * @param {string} type Name of signal.
      * @param {Receiver} receiver Function called when signal dispatches.
-     * @returns The instance.
      */
-    once(type: string, receiver: Receiver): this;
+    once(type: string, receiver: Receiver): void;
     /**
      * Removes a callback on a signal.
      * @param {string} type Name of signal.
      * @param {Receiver} receiver Subscriber function.
-     * @returns The instance.
      */
-    off(type: string, receiver: Receiver): this;
+    off(type: string, receiver: Receiver): void;
     /**
      * Sends a signal.
      * @param {string} type Name of signal.
      * @param {any[]} args Arguments to receiver.
-     * @returns The instance.
      */
-    emit(type: string, ...args: any[]): this;
+    emit(type: string, ...args: any[]): void;
     /**
      * Removes all signal callbacks.
-     * @returns The instance.
      */
-    reset(): this;
+    reset(): void;
     #private;
 }
 export type Receiver<A extends any[] = any[]> = (...args: A) => void;
